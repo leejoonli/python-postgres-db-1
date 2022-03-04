97b64a81d6268c58922530a2c4738a22ecff6c3a
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-04 15:29:09 -0800

    add locations model

--- python_postgres_exercise_1/models.py
+++ python_postgres_exercise_1/models.py
@@ -1,3 +1,4 @@
+from re import M
 from django.db import models
 
 # Create your models here.
@@ -13,3 +14,13 @@ class Countries(models.Model):
 
     def __str__(self):
         return self.name
+
+class Locations(models.Model):
+    street_address = models.CharField(max_length=100)
+    postal_code = models.IntegerField()
+    city = models.CharField(max_length=100)
+    state_province = models.CharField(max_length=100)
+    country_id = models.ForeignKey(Countries, on_delete=models.CASCADE, related_name='locations')
+
+    def __str__(self):
+        return (f'{self.street_address}, {self.city}, {self.state_province} {self.postal_code}')
