5249b0bd4ec467a9f1a5530efc075911a3548914
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-04 16:12:52 -0800

    add serializers

--- python_postgres_exercise_1/serializers.py
+++ python_postgres_exercise_1/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Region
 
 class RegionSerializer(serializers.HyperlinkedModelSerializer):
+    region = serializers.HyperlinkedRelatedField(view_name='region_detail', read_only=True)
     class Meta:
         model = Region
-        
+        fields = ('id', 'name', 'region')
--- python_postgres_exercise_1/views.py
+++ python_postgres_exercise_1/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RegionSerializer
+from .models import Region
 
 # Create your views here.
