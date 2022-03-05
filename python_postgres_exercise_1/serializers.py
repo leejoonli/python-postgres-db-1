380dcffccfa5f82497b26f1d77bd64371e8ca343
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-04 16:22:13 -0800

    add detail url to serializer

--- python_postgres_exercise_1/serializers.py
+++ python_postgres_exercise_1/serializers.py
@@ -3,6 +3,7 @@ from .models import Region
 
 class RegionSerializer(serializers.HyperlinkedModelSerializer):
     region = serializers.HyperlinkedRelatedField(view_name='region_detail', read_only=True)
+    region_url = serializers.ModelSerializer.serializer_url_field(view_name='region_detail')
     class Meta:
         model = Region
-        fields = ('id', 'name', 'region')
+        fields = ('id', 'name', 'region', 'region_url')
