a7d96d265e3c8ead0ef29b164284a5dc97d810a9
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-04 16:16:57 -0800

    add views

--- python_postgres_exercise_1/views.py
+++ python_postgres_exercise_1/views.py
@@ -4,3 +4,10 @@ from .serializers import RegionSerializer
 from .models import Region
 
 # Create your views here.
+class RegionList(generics.ListCreateAPIView):
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializer
+
+class RegionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializer
